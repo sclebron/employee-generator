96311d928e0c4dca8f6d9c3d3661eea62a7898e1
SLeBron <sophieclebron@gmail.com>
2022-10-06 21:37:30 -0600

    started adding seed data

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ USE employees_db;
 
 CREATE TABLE department (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    department_name VARCHAR(30) NOT NULL
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
