3007470e72d6c50cfd507a3f9462c2b46d32cfc2
SLeBron <sophieclebron@gmail.com>
2022-10-06 23:51:49 -0600

    updated server.js

--- server.js
+++ server.js
@@ -1,16 +1,3 @@
-const { appendFile } = require("fs");
-
-const PORT = process.env.PORT || 3001;
-const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: 'root',
-        password: 'Tigers',
-        database: 'employees_db'
-    },
-    console.log(`Connected to the employees_db database`)
-);
-
-app.listen(PORT, () => {
-    console.log(`Server now running on port ${PORT}`);
-});
+const connection = require('./config/connection');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
