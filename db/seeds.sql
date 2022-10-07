c5ba038f71f531d3117153bb3b89549b8691ade6
SLeBron <sophieclebron@gmail.com>
2022-10-06 22:45:15 -0600

    finished adding seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,4 +5,4 @@ INSERT INTO role(title, salary, department_id)
 VALUES ("Sales Lead", 100000, 1), ("Salesperson, 80000", 1), ("Lead Engineer", 150000, 2), ("Software Engineer", 120000, 2), ("Account Manager", 160000, 3), ("Accountant", 125000, 3), ("Legal Team Lead", 250000, 4), ("Lawyer", 190000, 4);
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUES 
+VALUES ("John", "Doe", 1, 2), ("Mike", "Chan", 2, null), ("Ashley", "Rodriguez", 1, 2), ("Kevin", "Tupik", 3, 3), ("Kunal", "Singh", 4, null);
